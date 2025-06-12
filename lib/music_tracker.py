127daa4e5b0a48d82f25b6dd27c591ad112fa246
alexnorniella <alexnorniella@yahoo.co.uk>
2025-06-12 14:56:46 +0100

    2nd test written

--- lib/music_tracker.py
+++ lib/music_tracker.py
@@ -1,2 +1,12 @@
 class MusicTracker:
-    pass
+
+    def __init__(self):
+        self.music_tracker = []
+    
+    def add_track(self,song):
+        self.music_tracker.append(song)
+
+    def list_tracks(self):
+        
+        return self.music_tracker
+
--- tests/test_music_tracker.py
+++ tests/test_music_tracker.py
@@ -1,14 +1,6 @@
-# """
-# Given a song,
-# It should add the song to the music_storage list
-# """
-
-# music_tracker = MusicTracker()
-# music_tracker.add_track('Numb')
-# music_tracker.list_tracks() # == ['Numb']
-
 from lib.music_tracker import *
 import pytest
+
 
 def test_given_a_song():
     music_tracker = MusicTracker()
@@ -16,18 +8,11 @@ def test_given_a_song():
     assert music_tracker.list_tracks() == ['Numb']
 
 
-# '''
-# Given 2 songs,
-# It should add both the songs to the list
-# '''
-
-# music_tracker = MusicTracker()
-# music_tracker.add_track('Numb')
-# music_tracker.add_track('Piano Man')
-# music_tracker.list_tracks() # == ['Numb','Piano Man']
-
-
-
+def test_given_two_songs():
+    music_tracker = MusicTracker()
+    music_tracker.add_track('Numb')
+    music_tracker.add_track('Piano Man')
+    assert music_tracker.list_tracks() == ['Numb', 'Piano Man']
 
 
 
